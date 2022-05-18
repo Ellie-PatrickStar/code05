b3df14e7ddb7416c986024aa3e239d5a56838a25
Ellie-PatrickStar <3308556960@qq.com>
2022-05-18 16:12:46 +0800

    20200518利用design工具完成了约束布局，使用alpha完成了透明度蒙版，但是报错不能运行，

--- app/src/main/java/com/example/code05/MainActivity.java
+++ app/src/main/java/com/example/code05/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.code05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TextView bg= findViewById(R.id.bg);//找到你想要设置透明背景的layou的id
+
+        //bg.getBackground().setAlpha(255);//0--255为透明度值
+
     }
 }
